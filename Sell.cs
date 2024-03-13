6c071caa10ed01affa6581a955ed48c97ab35aa5
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-03-13 01:26:15 -0300

    Agregar archivos de proyecto.

---
+++ Sell.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Sell
+{
+	public int id{ get; set; }
+	public DateTime date { get; set; } = DateTime.Now;
+	public string name { get; set; }
+	public double? price { get; set; }
+	public int transfer {  get; set; } = 0;
+}
